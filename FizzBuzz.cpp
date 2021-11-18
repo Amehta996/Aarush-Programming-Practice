1fb4f4bc66e585d585084b1d4f14cb0393190e2f
Aarush Mehta <93052292+Amehta996@users.noreply.github.com>
2021-11-18 07:10:43 -0500

    Update FizzBuzz.cpp
    Had Buzz in the If statement for modulo 3, replaced it with Fizz

--- FizzBuzz.cpp
+++ FizzBuzz.cpp
@@ -7,7 +7,7 @@ int main() {
     } else if(i % 5 == 0){
       std::cout << "Buzz\n";
     } else if(i % 3 == 0){
-      std::cout << "Buzz\n";
+      std::cout << "Fizz\n";
     } else {
       std::cout << i << "\n";
     }
