758c048e7fd08d86742a6e87ecece0f7ac076af0
Aarush Mehta <93052292+Amehta996@users.noreply.github.com>
2021-11-24 20:10:05 -0500

    Add files via upload
    Tried my hand at a 1000 difficulty on codeforces... Working my way up  very fast :D

---
+++ CodeForces 1A Theater Square.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+int main(){
+
+ 
+
+long long n,m,a;
+
+cin >> n >> m >> a;
+
+long long please = ceil((double)n/a)*ceil((double)m/a);
+cout << please;
+}
