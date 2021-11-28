0b3fe4a61e96921d7aa4d9f5e0564143db4e973b
Aarush Mehta <93052292+Amehta996@users.noreply.github.com>
2021-11-28 18:26:57 -0500

    Add files via upload
    My attempt at the first step towards learning dart using just documentation.

---
+++ experimenting .dart
@@ -0,0 +1,16 @@
+
+void main() {
+	
+String firstName = "Ananya";
+int bruh = 1;
+int grade = 10;
+
+//just doing the bare basics -- trying out using ${} to insert any variable types 
+print("My name is ${firstName}");
+print("I am in grade ${grade}");
+print("I am currently trying to learn the dart progamming language using only documentation!!!");
+print("Until next time... ${firstName}");
+
+
+
+}
