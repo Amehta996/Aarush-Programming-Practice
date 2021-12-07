2da5675d067b8906980fd9a0ccd93d41afad17eb
Aarush Mehta <93052292+Amehta996@users.noreply.github.com>
2021-12-07 09:32:10 -0500

    Add files via upload
    Practicing basic string manipulation. Getline to get the input of a full line into a string including the spaecs, and Contacetating two, or more strings into one.

---
+++ Getline.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main() {
+string name1;
+cin >> name1;
+cout << name1;
+
+string name;
+getline(cin >> ws, name);
+cout << name;
+
+} 
